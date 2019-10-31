7108fc599015df405a8a1f86f88ee4e293807e71
github username <github email address>
2019-10-31 13:50:17 -0500

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,4 @@
 class Author
     attr_accessor :name
-    def name=(name)
-        @name = name
-    end
+
 end
--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,4 @@
 class Post
     attr_accessor :title, :author
-    def title=(title)
-        @title = title
-    end
-    def author=(author)
-        @author = author
-    end
+
 end
